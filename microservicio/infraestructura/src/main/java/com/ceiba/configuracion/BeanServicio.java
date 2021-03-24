3eed34bbc1f1c0909ddfa3445e15de0912850472
Karen Estefany Lopez Segura - Ceiba Software <karen.lopez@ceiba.com.co>
2021-03-23 19:08:37 -0500

    remove usuario

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -5,31 +5,12 @@ import com.ceiba.reserva.servicio.ServicioCrearReserva;
 import com.ceiba.reserva.servicio.ServicioEliminarReserva;
 import com.ceiba.sala.puerto.repositorio.RepositorioSala;
 import com.ceiba.sala.servicio.ServicioActualizarSala;
-import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
-import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
-import com.ceiba.usuario.servicio.ServicioCrearUsuario;
-import com.ceiba.usuario.servicio.ServicioEliminarUsuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
 
-    @Bean
-    public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioCrearUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioEliminarUsuario servicioEliminarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioEliminarUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioActualizarUsuario(repositorioUsuario);
-    }
-    
     @Bean
     public ServicioActualizarSala servicioActualizarSala(RepositorioSala repositorioSala) {
         return new ServicioActualizarSala(repositorioSala);
